8d7a1a4236bc7263fcd4e0026b91d7004c0ce3d2
janelle <731364@student.dusd.net>
2024-01-23 13:50:03 -0800

    added a new script
    activating objects

---
+++ ScriptingProjectP5Aguayo/Assets/Scripts/ActiveObjects.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using UnityEngine;
+
+public class ActivatingGameObjects : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
