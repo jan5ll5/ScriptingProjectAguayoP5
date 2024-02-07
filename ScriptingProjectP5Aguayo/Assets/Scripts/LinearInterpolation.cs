c455e0620e1427b306402d4e3abc9f55527704f7
janelle <731364@student.dusd.net>
2024-02-07 13:35:33 -0800

    linear interpolation
    I added another script called linear interpolation

---
+++ ScriptingProjectP5Aguayo/Assets/Scripts/LinearInterpolation.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{ /*
+   // In this case, result = 4
+    float result = Math.Lerp (3f, 5f, 0.5f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        light.intensity = Mathf.Lerp(light.intensity,8f, 0.5f * Time.deltaTime;
+    }
+ */
+}
