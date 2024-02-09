f0fa29498c355f91985af890611a107c218cd4d1
janelle <731364@student.dusd.net>
2024-02-09 12:37:58 -0800

    Update LinearInterpolation.cs
    fixed the script

--- ScriptingProjectP5Aguayo/Assets/Scripts/LinearInterpolation.cs
+++ ScriptingProjectP5Aguayo/Assets/Scripts/LinearInterpolation.cs
@@ -6,6 +6,12 @@ public class LinearInterpolation : MonoBehaviour
 { /*
    // In this case, result = 4
     float result = Math.Lerp (3f, 5f, 0.5f);
+
+    Vector3 from = new Vector3 (1f, 2f, 3f);
+    Vector3 to = new Vector3 (5f, 6f, 7f);
+
+    // Here result = (4, 5, 6)
+    Vector3 result = Vector3.Lerp (from, to, 0.75f);
     // Start is called before the first frame update
     void Start()
     {
