c5b46b6c92bcb8fced441bc2967ef452586487cc
janelle <731364@student.dusd.net>
2024-02-14 12:41:10 -0800

    added a new script
    added 3 destroy scripts

---
+++ ScriptingProjectP5Aguayo/Assets/Scripts/DestroyBasic.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using UnityEngine;
+
+public class DestroyBasic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ ScriptingProjectP5Aguayo/Assets/Scripts/DestroyOther.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using UnityEngine;
+
+public class DestroyOther : MonoBehaviour
+{
+    public GameObject other;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Destroy(other);
+        }
+    }
+}
