9583131d9ced550415a97570965f5ac5436f3665
janelle <731364@student.dusd.net>
2024-02-21 12:40:48 -0800

    started new script
    created new script

---
+++ ScriptingProjectP5Aguayo/Assets/Scripts/KeyInput.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class KeyInput : MonoBehaviour
+{
+    public Image graphic;
+    public Sprite standard;
+    public Sprite downgfx;
+    public Sprite upgfx;
+    public Sprite heldgfx;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
