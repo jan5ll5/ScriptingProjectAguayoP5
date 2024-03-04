8cbb249d9c71b1b29f08ac1a89139ce6414864fe
janelle <731364@student.dusd.net>
2024-03-04 13:29:51 -0800

    get axis
    i finished 3 script on get axis

---
+++ ScriptingProjectP5Aguayo/Assets/Scripts/AxisRawExample.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AAxisRawExample : MonoBehaviour
+{
+    public float range;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float xPos = h * range;
+
+        transform.position = new Vector3(xPos, 2f, 0);
+        Debug.Log("Value Returned: " + h.ToString("F2"));
+    }
+}
