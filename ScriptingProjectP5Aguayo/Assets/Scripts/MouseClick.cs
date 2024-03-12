12d555cae90902a4b36483fb9a9aa91e8cbf5700
janelle <731364@student.dusd.net>
2024-03-12 12:59:50 -0700

    OnMouseDown
    created a new script

---
+++ ScriptingProjectP5Aguayo/Assets/Scripts/MouseClick.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClick : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnMouseDown()
+    {
+        rb.AddForce(-transform.forward * 500f);
+        rb.useGravity = true;
+    }
+
+}
