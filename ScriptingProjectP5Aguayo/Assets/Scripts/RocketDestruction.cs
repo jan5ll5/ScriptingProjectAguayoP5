c997c4aab997421afcf4af34d9fcfedf80e701d9
janelle <731364@student.dusd.net>
2024-04-12 14:08:02 -0700

    fixed classes and instantiate
    i fixed classes and i finished instantiate

---
+++ ScriptingProjectP5Aguayo/Assets/Scripts/RocketDestruction.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using UnityEngine;
+
+public class RocketDestruction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 1.5f);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ScriptingProjectP5Aguayo/Assets/Scripts/UsingInstantiate.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using UnityEngine;
+
+public class UsingInstantiate : MonoBehaviour
+{
+    public Rigidbody rocketPrefab;
+    public Transform barrelEnd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody rocketInstance;
+            rocketInstance = Instantiate(rocketPrefab, barrelEnd.position, barrelEnd.rotation) as Rigidbody;
+            rocketInstance.AddForce(barrelEnd.forward * 5000);
+        }
+    }
+}
