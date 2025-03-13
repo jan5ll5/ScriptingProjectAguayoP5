696d56d036d42cc4ae9831d9fa82e643d5a18aea
janelle <145067236+jan5ll5@users.noreply.github.com>
2025-03-13 10:37:51 -0700

    quaternion
    quaternion

---
+++ ScriptingProjectP5Aguayo/Assets/Quaternions/LookAtScript4.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using UnityEngine;
+
+public class LookAtScript4 : MonoBehaviour
+{
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 relativePos = target.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(relativePos);
+    }
+}
---
+++ ScriptingProjectP5Aguayo/Assets/Quaternions/Some7Class.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using UnityEngine;
+
+public class Some7Class : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.rotation = Quaternion.identity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
